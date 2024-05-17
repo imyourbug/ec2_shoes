adcb43238670c1929f66d04c3f36914e4f47079b
imyourbug <72309180+imyourbug@users.noreply.github.com>
2024-05-17 07:44:11 +0000

    fix docker

--- fe/README.md
+++ fe/README.md
@@ -1,21 +1,8 @@
+# fe
+
 ## Project setup
 ```
-npm i
-npm i vue3-google-oauth2 --save
-npm i vue3-storage --save
-npm i vue3-route@next --save
-npm i bootstrap --save
-npm i @popperjs/core --save
-npm i axios --save
-npm i --save mitt
-npm i --save url-loader --save
-npm i ntc-hi-js --save
-npm i vue3-select2-component --save
-npm i mitt --save
-npm i @vuelidate/core @vuelidate/validators --save
-npm i vue-dk-toast --save
-npm i @vue-stripe/vue-stripe --save
-npm i @vueuse/core --save
+npm install
 ```
 
 ### Compiles and hot-reloads for development
--- fe/src/main.js
+++ fe/src/main.js
@@ -1,35 +1,4 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
-import '@hoppscotch/vue-toasted/style.css';
+import { createApp } from 'vue'
+import App from './App.vue'
 
-import { createApp } from 'vue';
-
-import mitt from 'mitt';
-import * as uiv from 'uiv';
-import vue3GoogleLogin from 'vue3-google-login';
-import Select2 from 'vue3-select2-component';
-import Vue3Tab from 'vue3-tab';
-
-import store from '@/store';
-import Toasted from '@hoppscotch/vue-toasted';
-
-import App from './App.vue';
-import router from './routes';
-
-const emitter = mitt();
-const app = createApp(App);
-const gauthClientId = process.env.VUE_APP_GOOGLE_OAUTH_CLIENT_ID;
-
-app.provide("emitter", emitter);
-app.use(Toasted); // You can pass options as a second parameter
-app.use(store);
-app.use(router);
-app.use(uiv);
-app.use(Vue3Tab);
-app.use(vue3GoogleLogin, {
-  clientId: gauthClientId,
-});
-
-app.component("Select2", Select2);
-
-app.mount("#app");
+createApp(App).mount('#app')
